46be5cd7893c145d324a345c0c1de0eaf536bc5e
TJ Hardin <MrT246007@gmail.com>
2019-06-11 02:40:21 -0500

    Added Styling to Navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+import "./App.css";
 
 class App extends Component {
   render () {
     return (
       <BrowserRouter>
         <div className="App">
-          <nav>
+          <nav className="Navigation">
             <p>Courses</p>
             <p>Users</p>
           </nav>
--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "../Courses/Courses.css";
 
 class Users extends Component {
     render () {
