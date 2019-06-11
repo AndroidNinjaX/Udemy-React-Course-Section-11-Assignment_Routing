f3d783234e78a7b929d53a8faa0d1ca1c099e850
TJ Hardin <MrT246007@gmail.com>
2019-06-11 02:08:35 -0500

    Complete first objective

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <Route path="/courses" component={Courses} />
           <Route path="/users" component={Users} />
           <ol style={{textAlign: 'left'}}>
-            <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
+            <li><b>COMPLETE! </b>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
             <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
             <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
             <li>Pass the course ID to the "Course" page and output it there</li>
