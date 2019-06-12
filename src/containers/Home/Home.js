a5998903359e718847b3be6a1d4c0e3f7613bc99
TJ Hardin <MrT246007@gmail.com>
2019-06-12 02:26:47 -0500

    Create a "home" component

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//This component is to display a welcome page.
+const Home = () => (
+    <div>
+        <h1>Welcome to the application!</h1>
+    </div>
+);
+
+export default Home;
