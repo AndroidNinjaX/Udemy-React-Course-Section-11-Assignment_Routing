4de45532b0c7a624ef7ff7b5b48aff60fc03ea41
TJ Hardin <MrT246007@gmail.com>
2019-06-12 02:17:47 -0500

    Route to Objectives component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+import Objectives from './containers/Objectives/Objectives';
 import "./App.css";
 
 class App extends Component {
@@ -41,20 +42,11 @@ class App extends Component {
             <Route path="/" exact render={() => <h1>Home</h1>} />
             <Route path="/courses" component={Courses} />
             <Route path="/users" component={Users} />
+            <Route path="/objectives" component={Objectives} />
             <Redirect from="/all-courses" to="/courses" />
             <Route render={() => <h1>404 Not Found</h1>} />
           </Switch>
-
-          <ol style={{textAlign: 'left'}}>
-            <li><b>COMPLETE! </b>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-            <li><b>COMPLETE! </b>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-            <li><b>COMPLETE! </b>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-            <li><b>COMPLETE! </b>Pass the course ID to the "Course" page and output it there</li>
-            <li><b>COMPLETE! </b>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-            <li><b>COMPLETE! </b>Load the "Course" component as a nested component of "Courses"</li>
-            <li><b>COMPLETE! </b>Add a 404 error page and render it for any unknown routes</li>
-            <li><b>COMPLETE! </b>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
-          </ol>
+          
         </div>
       </BrowserRouter>
     );
