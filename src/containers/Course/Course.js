22a1e55a5beced30ae7f05aa29a27ed661944f61
TJ Hardin <MrT246007@gmail.com>
2019-06-12 01:26:21 -0500

    Comments

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -25,25 +25,37 @@ class Course extends Component {
     render () {
 
         /*------------------
-        To display the Title as a "prop" or a "param".
-        -First set a variable as "null".
+        To display the Title if I have as a "prop" or a "param".
+        -First set a variable as "null", in this case "title".
         -Check if there is a "props.courseInfo".
-        -If so, set "title" to the title in "courseInfo".
+        -If so, set "title" to the title in "courseInfo.title".
         -If not, set "title" to "param.title". 
             -This helps when you click on just 1 course, and have only "params".
+
+        Also create another variable called "output" in this instance.
+        -If I have "props", then I can also output the "ID".
+        -If not, then I do not output the "ID" just the "title".
+        -Either way, set "output" to some JSX that is outputted.
         ------------------*/
         let title = null;
+        let output = null;
         
         if(this.props.courseInfo){
             title = this.props.courseInfo.title;
+            output = <div>
+                        <h1>{title}</h1>
+                        <p>You selected the Course with ID: {this.props.courseID}</p>
+                    </div>;
         } else {
             title = this.props.match.params.title;
+            output = <div>
+                        <h1>{title}</h1>
+                    </div>;
         }
 
         return (
             <div className="Course">
-                <h1>{title}</h1>
-                <p>You selected the Course with ID: {this.props.courseID}</p>
+                {output}
             </div>
         );
     }
